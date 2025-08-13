f2ceec736ccb850f32594eb8b46813bc4edc3875
xwuu0117 <xwuu0117@student.monash.edu>
2025-08-13 11:56:19 +0800

    Trigger deployment

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/xwuu0117/ProjectForCloudflare/'
-  : '/'
-})
-// module.exports = {
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
 //   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/xwuu0117/ProjectForCloudflare/'
-//     : '/'
-// }
+//   ? '/xwuu0117/ProjectForCloudflare/'
+//   : '/'
+// })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/xwuu0117/ProjectForCloudflare/'
+    : '/'
+}
